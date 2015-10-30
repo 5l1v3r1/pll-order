872f147557dadc6eb38333d847228a0b7329b127
Alex Nichol <unixpickle@gmail.com>
2015-10-30 15:22:52 +0000

    added surface turns to moves

--- src/pll_order/core_moves.clj
+++ src/pll_order/core_moves.clj
@@ -1,23 +1,33 @@
 (ns pll-order.core)
 
-(defn- four-cycles
-  "Turn a sequencial list of stickers into three four-cycles."
+(defn- ring-cycles
   [stickers]
   (multicycle (take-nth 3 stickers)
               (take-nth 3 (drop 1 stickers))
               (take-nth 3 (drop 2 stickers))))
 
+(defn- surface-cycles
+  [stickers]
+  (multicycle (take-nth 2 stickers)
+              (take-nth 2 (drop 1 stickers))))
+
 (defn- face-permutation
-  "Perform a permutation on a face given its name"
+  "Generate a permutation for a clockwise turn of the given face"
   [face]
-  ;; rings is a list of sticker indices which each move applies, in a ring-like order.
   (let [rings {\R '(8 5 2 27 30 33 17 14 11 26 23 20)
                \L '(0 3 6 18 21 24 9 12 15 35 32 29)
                \F '(6 7 8 36 39 42 11 10 9 53 50 47)
                \B '(3 2 1 45 48 51 15 16 17 44 41 38)
                \U '(20 19 18 47 46 45 29 28 27 38 37 36)
-               \D '(24 25 26 42 43 44 33 34 35 51 52 53)}]
-    (four-cycles (get rings face))))
+               \D '(24 25 26 42 43 44 33 34 35 51 52 53)}
+        surfaces {\R '(36 37 38 41 44 43 42 39)
+                  \L '(45 46 47 50 53 52 51 48)
+                  \F '(18 19 20 23 26 25 24 21)
+                  \B '(27 28 29 32 35 34 33 30)
+                  \U '(0 1 2 5 8 7 6 3)
+                  \D '(9 10 11 14 17 16 15 12)}]
+    (comp (ring-cycles (get rings face))
+          (surface-cycles (get surfaces face)))))
 
 (defn- move-permutation
   "Turn a move symbol into a permutation"
