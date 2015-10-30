a082b0d826fa432464e012dd96c48655c1b3faf8
Alex Nichol <unixpickle@gmail.com>
2015-10-29 22:09:55 -0400

    drafted algorithm parser

--- src/pll_order/core_plls.clj
+++ src/pll_order/core_plls.clj
@@ -1,32 +1,42 @@
 (ns pll-order.core)
 
-(def ^:const EdgeL 0)
-(def ^:const EdgeB 1)
-(def ^:const EdgeR 2)
-(def ^:const EdgeF 3)
-(def ^:const CornerBL 4)
-(def ^:const CornerBR 5)
-(def ^:const CornerFR 6)
-(def ^:const CornerFL 7)
+(defn four-cycles
+  "Turn a sequencial list of stickers into three four-cycles."
+  [stickers]
+  (multicycle (take-nth 3 stickers)
+              (take-nth 3 (drop 1 stickers))
+              (take-nth 3 (drop 2 stickers))))
 
-(defn auf-moves
-  "Perform a certain number of U moves to a permutation function"
- [count perm]
-  (let [u-move (multicycle [0 1 2 3] [4 5 6 7])]
-    (nth (iterate (comp u-move) identity-perm) count)))
+(defn face-permutation
+  "Perform a permutation on a face given its name"
+  [face]
+  ;; rings is a list of sticker indices which each move applies, in a ring-like order.
+  (let [rings {\R '(8 5 2 27 30 33 17 14 11 26 23 20)
+               \L '(0 3 6 18 21 24 9 12 15 35 32 29)
+               \F '(6 7 8 36 39 42 11 10 9 53 50 47)
+               \B '(3 2 1 45 48 51 15 16 17 44 41 38)
+               \U '(20 19 18 47 46 45 29 28 27 38 37 36)
+               \D '(24 25 26 42 43 44 33 34 35 51 52 53)}]
+    (four-cycles (get rings face))))
+
+(defn move-permutation
+  "Turn a move symbol into a permutation"
+  [moveSymbol]
+  (let [s (str moveSymbol)]
+    (cond (= 1 (count s))
+          (face-permutation s)
+          (= \2 (get s 1))
+          (comp (face-permutation s) (face-permutation s))
+          :else
+          (comp (face-permutation s) (face-permutation s) (face-permutation s)))))
+
+(defn alg-permutation
+  "Turn a list of symbols representing moves into a permutation"
+  [& moves]
+  (if (seq moves)
+      (comp (move-permutation (first moves)) (apply alg-permutation (rest moves)))
+      identity-perm))
 
 (def plls {
-  :h (multicycle [EdgeL EdgeR] [EdgeF EdgeB])
-  :z (multicycle [EdgeF EdgeL] [EdgeB EdgeR])
-  :u1 (cycle-perm  EdgeL EdgeF EdgeR)
-  :u2 (cycle-perm  EdgeR EdgeF EdgeL)
-  :a1 (cycle-perm  CornerBL CornerBR CornerFR)
-  :a2 (cycle-perm  CornerFR CornerBR CornerBL)
-  :e (multicycle [CornerFR CornerFL] [CornerBR CornerBL])
-  :f (multicycle [CornerFR CornerBR] [EdgeF EdgeB])
-  :g1 (auf-moves 1 (multicycle [CornerFR CornerFL CornerBL] [EdgeF EdgeB EdgeL]))
-  :g2 (multicycle [CornerBL CornerFL CornerFR] [EdgeF EdgeL EdgeB])
-  ;; TODO: add auf-moves to the next two perms
-  :g3 (multicycle [CornerBR CornerFL CornerFR] [EdgeF EdgeB EdgeR])
-  :g4 (multicycle [CornerFL CornerBR CornerFR] [EdgeB EdgeF EdgeR])
+  :h (alg-permutation '(L R U2 L' R' F' B' U2 F B))
 })
