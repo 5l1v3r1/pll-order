ec2d0ea99fa8adac58cac9f2b494b1e30edacff6
Alex Nichol <unixpickle@gmail.com>
2015-10-30 22:38:20 -0400

    compute solutions!

--- src/pll_order/core.clj
+++ src/pll_order/core.clj
@@ -3,7 +3,24 @@
 (load "core_moves")
 (load "core_plls")
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- scramble-sequence
+  [s]
+  (if (<= (count s) 1) s
+      (let [el (rand-nth s)]
+        (cons el (scramble-sequence (remove #(= el %) s))))))
+
+(defn find-identity-perm
+  []
+  (let [s0 (keys plls)]
+    (loop []
+      (let [s (scramble-sequence s0)
+            perms (map plls s)
+            cube (apply comp perms)]
+        (if (cubes-equal identity-perm cube) s (recur))))))
+
+(defn -main
+  "Find an order of PLLs to arrive at the identity."
+  []
+  (loop []
+    (println (map #(subs (str %) 1) (find-identity-perm)))
+    (recur)))
