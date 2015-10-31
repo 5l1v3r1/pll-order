36910db081997be7587f88f540d44f305543c57e
Alex Nichol <unixpickle@gmail.com>
2015-10-31 10:18:54 -0400

    compute percent of solved permutations

--- src/pll_order/core.clj
+++ src/pll_order/core.clj
@@ -18,9 +18,22 @@
             cube (apply comp perms)]
         (if (cubes-equal identity-perm cube) s (recur))))))
 
+(defn compute-percent-solved
+  []
+  (loop [i 1000 c 0]
+    (if (zero? i)
+        (float (/ c 10))
+        (let [s (scramble-sequence (keys plls))
+              perms (map plls s)
+              cube (apply comp perms)]
+          (if (cubes-equal identity-perm cube)
+              (recur (dec i) (inc c))
+              (recur (dec i) c))))))
+
 (defn -main
   "Find an order of PLLs to arrive at the identity."
   []
-  (loop []
-    (println (map #(subs (str %) 1) (find-identity-perm)))
-    (recur)))
+  (println "Finding an identity order...")
+  (println (map #(subs (str %) 1) (find-identity-perm)))
+  (println "Computing statistics...")
+  (println "Roughly" (compute-percent-solved) "% are solved."))
